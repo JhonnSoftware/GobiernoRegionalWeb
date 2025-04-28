6d4459c1457181d70c166a9971c9c66493ac1621
Jhonn <72659574@continental.edu.pe>
2025-04-28 00:18:07 -0500

    V44

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -247,17 +247,6 @@
                 <img src="images/portaltranspte.png" alt="portaltransferecia" style="margin-right: 10px;">
             </a>
         </div>
-        <div class="britransmission">
-            <a href="https://www.regionjunin.gob.pe/pagina/id/region_en_accion/">
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="50" height="50"
-                    fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M6 2c-1.10457 0-2 .89543-2 2v4c0 .55228.44772 1 1 1s1-.44772 1-1V4h12v7h-2c-.5523 0-1 .4477-1 1v2h-1c-.5523 0-1 .4477-1 1s.4477 1 1 1h5c.5523 0 1-.4477 1-1V3.85714C20 2.98529 19.3667 2 18.268 2H6Z" />
-                    <path
-                        d="M6 11.5C6 9.567 7.567 8 9.5 8S13 9.567 13 11.5 11.433 15 9.5 15 6 13.433 6 11.5ZM4 20c0-2.2091 1.79086-4 4-4h3c2.2091 0 4 1.7909 4 4 0 1.1046-.8954 2-2 2H6c-1.10457 0-2-.8954-2-2Z" />
-                </svg>
-            </a>
-        </div>
 
     </header>
 
@@ -336,17 +325,17 @@
             <p>PROGRAMA DE<br>VOLUNTARIADO-MIMP</p>
         </div>
         <div class="bributton-container">
-            <a href="{{ url('obras_en_progreso') }}"
-                class="circle-button"><i class="fa-solid fa-oil-well"></i></a>
-            <p>OBRAS EN<br>PROGRESO</p>
+            <a href="http://sisdore.regionjunin.gob.pe:8080/convocatoria/"
+                class="circle-button"><i class="fa-solid fa-briefcase"></i></a>
+            <p>CONVOCATORIAS</p>
         </div>
     </section>
 
     <!-- BOLETINES INFORMATIVOS (FRANJA) -->
     <article>
         <h1 class="boletines">BOLETINES</h2>
-            <h2 class="informativos">informativos</h3>
-                <hr>
+        <h2 class="informativos">informativos</h3>
+        <hr>
     </article>
 
     <!-- SECCION BOLETINES -->
